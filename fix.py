6b85a3ddb0f97012c665a9aee4956b5622003843
sage <sage.b.shaw@gmail.com>
2018-05-27 02:44:15 -0600

    before fix.py for sidebar fix

---
+++ fix.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import glob
+
+string_start = '<div class="divSidebar"'
+string_end = '</div>'
+new_string = '<div class="divSidebar" w3-include-html="sidebar.html">\n'
+
+target_dir = 'test'
+
+if __name__ == '__main__':
+
+    
+
+    for file_path in glob.iglob(target_dir + '**/*.html', recursive=True):
+        f = open(file_path, 'r')
+        full_html = f.read()
+        f.close()
+
+        begin = full_html.find(string_start)
+        if begin == -1:
+            continue
+
+        end = full_html.find(string_end, begin)
+
+        new_html = full_html[:begin] + new_string + full_html[end:]
+        f = open(file_path, 'w')
+        f.write(new_html)
+        f.close()
+
+        print(file_path)
