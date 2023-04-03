3e9ca097ddd9661a5c5d4f13d303c38babf159df
Sage <sage.b.shaw@gmail.com>
2023-04-02 19:57:10 -0600

    comps presnetation draft done

--- presentations/20230404_comps/media/apparent_motion_demo.py
+++ presentations/20230404_comps/media/apparent_motion_demo.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+from tqdm import tqdm
+
+# Apparent motion comparison
 FILE_NAME = 'apparent_motion_demo.gif'
 
 dist_fast = 1/31
@@ -15,6 +18,7 @@ def effective_time(t, dist):
     return np.floor(t/dist)*dist
 
 ts = np.linspace(0, 1, 151)
+
 
 fig = plt.figure(figsize=(7, 3))
 plt.tight_layout()
@@ -35,6 +39,87 @@ with imageio.get_writer(FILE_NAME, mode='I', duration=.02) as writer:
         smooth.set_xdata(t)
         fast.set_xdata(effective_time(t, dist_fast))
         slow.set_xdata(effective_time(t, dist_slow))
+        plt.savefig(FILE_NAME + '.png')
+        image = imageio.imread(FILE_NAME + '.png')
+        writer.append_data(image)
+        plt.pause(0.01)
+
+plt.close()
+os.remove(FILE_NAME + '.png')
+
+# Constant moving
+FILE_NAME = 'smooth_dot.gif'
+dist_fast = 1/31
+dist_slow = 1/5
+ts = np.linspace(0, 1, 151)
+
+fig = plt.figure(figsize=(7, 3))
+plt.tight_layout()
+smooth, = plt.plot(0, 2, 'ro', markersize=30)
+plt.xlim(-.1, 1.2)
+# plt.ylim(-1.1, 2.3)
+plt.axis('off')
+plt.tight_layout()
+with imageio.get_writer(FILE_NAME, mode='I', duration=.02) as writer:
+    for t in tqdm(ts):
+        smooth.set_xdata(t)
+        plt.savefig(FILE_NAME + '.png')
+        image = imageio.imread(FILE_NAME + '.png')
+        writer.append_data(image)
+        plt.pause(0.01)
+
+plt.close()
+os.remove(FILE_NAME + '.png')
+
+# Flashing
+FILE_NAME = 'flashing_dot.gif'
+dist_fast = 1/31
+dist_slow = 1/5
+ts = np.linspace(0, 1, 151)
+
+freq = 20
+
+def ydata(t):
+    return np.sign(np.sin(t*2*np.pi*freq))
+
+fig = plt.figure(figsize=(7, 3))
+plt.tight_layout()
+smooth, = plt.plot(0, 0, 'ro', markersize=30)
+plt.xlim(-.1, 1.2)
+plt.ylim(.9, 1.1)
+plt.axis('off')
+plt.tight_layout()
+with imageio.get_writer(FILE_NAME, mode='I', duration=.02) as writer:
+    for t in tqdm(ts):
+        smooth.set_xdata(t)
+        smooth.set_ydata(ydata(t))
+        plt.savefig(FILE_NAME + '.png')
+        image = imageio.imread(FILE_NAME + '.png')
+        writer.append_data(image)
+        plt.pause(0.01)
+
+plt.close()
+os.remove(FILE_NAME + '.png')
+
+# Apparent
+FILE_NAME = 'apparent_dot.gif'
+ts = np.linspace(0, 1, 151)
+freq = 20
+
+
+def effective_time(t):
+    return np.floor(t*freq)/freq
+
+fig = plt.figure(figsize=(7, 3))
+plt.tight_layout()
+smooth, = plt.plot(0, 1, 'ro', markersize=30)
+plt.xlim(-.1, 1.2)
+plt.ylim(.9, 1.1)
+plt.axis('off')
+plt.tight_layout()
+with imageio.get_writer(FILE_NAME, mode='I', duration=.02) as writer:
+    for t in tqdm(ts):
+        smooth.set_xdata(effective_time(t))
         plt.savefig(FILE_NAME + '.png')
         image = imageio.imread(FILE_NAME + '.png')
         writer.append_data(image)
